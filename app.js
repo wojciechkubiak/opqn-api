4d756c823e5a45c4fe6f18d3595e23a2fa7d678a
wojciechkubiak <wgkubiak@gmail.com>
2020-05-14 02:36:59 +0200

    Foreign keys

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ import Exam from "./models/exams";
 const app = express();
 
 Patron.hasMany(Protege);
-Exam.belongsTo(Protege);
-Protege.hasMany(Exam);
+Exam.belongsTo(Protege, {foreignKey: 'id'});
+Protege.hasMany(Exam, {foreignKey: 'protegeId'});
 
 app.use(cors());
 app.use(logger('dev'));
