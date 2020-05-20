c27e99b9fb3ee01dcdf6f2be408127cc4283fa20
wojciechkubiak <wgkubiak@gmail.com>
2020-05-20 13:53:09 +0200

    Revert "Revert "GET Proteges/Exams FOR Patrons""
    This reverts commit 1c2c2ac9433f0258210ca2143c96f20cdfecfaf2.

---
+++ controllers/patron-methods.js
@@ -0,0 +1,31 @@
+import Protege from "./../models/protege";
+import Patron from "./../models/patron";
+import Exams from "./../models/exams";
+import sequelize from "sequelize";
+
+exports.getPatronsData = (require, result, next) => {
+  const id = require.user.id;
+
+  Patron.findAll({
+    //   limit: 2,
+    include: [
+      {
+        model: Protege,
+        include: [
+          {
+            model: Exams,
+            where: {
+              id: id,
+            },
+          },
+        ],
+      },
+    ],
+  })
+    .then((patron) => {
+      result.status(200).json(patron);
+    })
+    .catch((error) => {
+      result.status(400).json({ error: error });
+    });
+};
